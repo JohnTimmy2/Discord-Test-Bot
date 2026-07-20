ad37152a7bc6ff935b9389b4d45bb124fa6c68a1
JohnWimble <bouydara91@gmail.com>
2026-07-21 00:39:38 +0700

    cooldown in intcreate

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,5 +1,28 @@
-const { Events, MessageFlags } = require('discord.js');
+const { Events, MessageFlags, Collection } = require('discord.js');
 
+const { cooldowns } = interaction.client;
+
+if (!cooldowns.has(cooldowns.data.name)) {
+    cooldowns.set(command.data.name, new Collection());
+}
+
+const now = Date.now();
+const timestamps = cooldowns.get(command.data.name);
+const defaultCooldownDuration = 3;
+const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1_000;
+if (timestamps.has(interaction.user.id)) {
+    const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+
+    if (now < cooldownTime) {
+        const expiredTimestamp = Math.round(expirationTime / 1_000);
+        return interaction.reply({
+            content: `Please wait, you are on a cooldown for \`${cooldown.data.name}\`. You can use it again <t:${expirationTimestamp}:R>.`,
+            flags: MessageFlags.Ephemeral,
+        });
+    }
+}
+timestamps.set(interaction.user.id, now);
+setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
 module.exports = {
 	name: Events.InteractionCreate,
 	async execute(interaction) {
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
 const { token } = require('./config.json');
+client.cooldowns = new Collection();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
