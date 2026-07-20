12e1841aa15ab0fb591ca1b010070c3e1535cf25
JohnWimble <bouydara91@gmail.com>
2026-07-20 18:11:48 +0700

    command files and stuff

---
+++ index.js
@@ -0,0 +1,39 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client ({intents: [GatewayIntentBits]});
+client.once(Events.ClientReady, (readyClient) => {
+    console.log(`Ready Logged in as ${readyClient.user.tag}`)
+
+});
+client.login(token);
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders){
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+    for (const file of commandFiles){
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command){
+            client.commands.set(command.data.namem, command);
+        } else { 
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
+
+
+client.on(Events.Interaction.Create, (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+    const command = interaction.client.commands.get(interaction.commandName);
+    if (!command){
+        console.error(`No command `)
+    }
+    console.log(interaction);
+});
