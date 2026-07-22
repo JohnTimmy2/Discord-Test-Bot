b5e9ad7e0288d2954a7ac91a4609fbb0c864a5b8
JohnWimble <bouydara91@gmail.com>
2026-07-22 23:40:19 +0700

    more in guide

--- Commands/Utility/guide.js
+++ Commands/Utility/guide.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder, Interaction } = require ('discord.js');
 const { execute } = require('../../events/interactionCreate');
+const interactionCreate = require('../../events/interactionCreate');
 
 module.exports = {
 data = new SlashCommandBuilder()
@@ -10,15 +11,23 @@ data = new SlashCommandBuilder()
         .setDescription('Phrase to search for')
         .setAutocomplete(true)),
     async autocomplete(Interaction){
-        const focusedValue = interaction.options.getFocused();
-        const choices = [
-            'Popular Topics: Threads',
-            'Sharding: Getting started',
-			'Library: Voice Connections',
-			'Interactions: Replying to slash commands',
-			'Popular Topics: Embed preview',
-        ];
+        const focusedOption = interaction.options.getfocused(true);
+        let choices;
+
+        if (focusedOption.name === 'query') {
+            choices = [
+                'Popular Topics: Threads',
+				'Sharding: Getting started',
+				'Library: Voice Connections',
+				'Interactions: Replying to slash commands',
+				'Popular Topics: Embed preview',
+            ];
+        }
+        if (focusedOption.name === 'version') {
+            choices = ['v9', 'v11', 'v12', 'v13', 'v14'];
+        }
         const filtered = choices.filter((choice) => choice
-            .startsWith(focusedValue));
+            .startsWith(focusedOption.value));
+            await interaction.respond(filtered.map((choice) => ({ name: choice, value: choice})));
     },
 };
